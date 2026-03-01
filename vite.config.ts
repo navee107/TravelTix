2a3de5f3c619b8e10ff37a9de10f9b0324a230a0
navee107 <147652089+navee107@users.noreply.github.com>
2026-03-02 01:38:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TravelTix/',   // IMPORTANT
+  base: '/TravelTix/', 
 })
 
 export default defineConfig(({mode}) => {
